ac6875e744866cff810aaa3ff37de107ef8061ca
tzx666 <916196773@qq.com>
2020-05-25 00:30:44 +0800

    搭好demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,7 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <el-link href="https://element.eleme.io" target="_blank">默认链接</el-link>
   </div>
 </template>
 
